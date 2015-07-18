587f8889272e874208a8b2af19df0cebcd457801
masaki_nukaga <masaki.nukaga1209@gmail.com>
2015-07-18 22:29:47 +0900

    ThemeモデルのAPI追加

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,2 +1,17 @@
 class Theme < ActiveRecord::Base
+
+  def index
+    @themes = Theme.all
+  end
+
+  def create
+    @theme = Theme.new(theme_params)
+  end
+
+  private
+
+  def theme_params
+    params.require(:theme).permit(:content, :kind)
+  end
+
 end
