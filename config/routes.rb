1e826997e4d73d9c9df6f7b29e59cf78f32a1944
masaki_nukaga <masaki.nukaga1209@gmail.com>
2015-07-19 04:29:13 +0900

    Themeリソースのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root 'theme#index'
+  root 'themes#index'
   resources :themes
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
