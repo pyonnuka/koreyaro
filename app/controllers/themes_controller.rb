600d06d7d3026e021c74680f9475131e290ca7d4
masaki_nukaga <masaki.nukaga1209@gmail.com>
2015-08-03 06:27:57 +0900

    テーマを決定するコントローラ作成

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -10,10 +10,16 @@ class ThemesController < ApplicationController
     render :index
   end
 
-  def decision
+  def decision_candidate
     themes = Theme.where("status = ?",'todo')
     theme_num = rand(themes.count) + 1
     @theme = themes[theme_num]
+  end
+
+  def decision
+    @theme = Theme.find(params[:id])
+    decision_theme = @theme
+    @theme.delete
   end
 
   private
